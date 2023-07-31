926bb59e1170eacd5c4bbdc42325dd58a821d90b
okayu0220 <y.okajima.0220@gmail.com>
2023-07-31 15:51:27 +0900

    Userモデルにフォーマットのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,      presence: true
-  validates :first_name_fw, presence: true
-  validates :last_name_fw,  presence: true
-  validates :first_name_kn, presence: true
-  validates :last_name_kn,  presence: true
+  validates :first_name_fw, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} # 全角の漢字・ひらがな・カタカナのみ許可
+  validates :last_name_fw,  presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} # 全角の漢字・ひらがな・カタカナのみ許可
+  validates :first_name_kn, presence: true, format: {with: /\A[ァ-ヶー]+\z/} # 全角のカタカナのみ許可
+  validates :last_name_kn,  presence: true, format: {with: /\A[ァ-ヶー]+\z/} # 全角のカタカナのみ許可
   validates :birthday,      presence: true
+  validates :password,      format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i} # 英数字混合
 end
