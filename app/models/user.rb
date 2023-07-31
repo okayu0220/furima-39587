b2afbabdc1c16e75a151208ce479c20ebb9a4d23
okayu0220 <y.okajima.0220@gmail.com>
2023-07-31 16:24:22 +0900

    バリデーションの記載順を変更
    エラーメッセージの表示順調整のため

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :password,      format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i} # 英数字混合
   validates :nickname,      presence: true
   validates :first_name_fw, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} # 全角の漢字・ひらがな・カタカナのみ許可
   validates :last_name_fw,  presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} # 全角の漢字・ひらがな・カタカナのみ許可
   validates :first_name_kn, presence: true, format: {with: /\A[ァ-ヶー]+\z/} # 全角のカタカナのみ許可
   validates :last_name_kn,  presence: true, format: {with: /\A[ァ-ヶー]+\z/} # 全角のカタカナのみ許可
   validates :birthday,      presence: true
-  validates :password,      format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i} # 英数字混合
 end
