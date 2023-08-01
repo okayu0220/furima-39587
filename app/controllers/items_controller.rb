5250599d859de4e90b278d812a1914f28ea73dbd
okayu0220 <y.okajima.0220@gmail.com>
2023-08-01 18:28:41 +0900

    出品フォーム実装と出品アイテム保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,30 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
+  private
+  def item_params
+    params.require(:item).permit(
+      :image,
+      :name,
+      :info,
+      :category_id,
+      :item_status_id,
+      :fee_status_id,
+      :prefecture_id,
+      :delivery_schedule_id,
+      :price
+    ).merge(user_id: current_user.id)
+  end
 end
