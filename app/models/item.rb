8d3740f2c0adec21f5e53711d9568709a5dc3e26
okayu0220 <y.okajima.0220@gmail.com>
2023-08-01 16:35:54 +0900

    ActiveStorageとのアソシエーションとバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,9 @@
 class Item < ApplicationRecord
 
   belogs_to :user
+  has_one_attached :image
 
+  validates :image,                presence: true
   validates :name,                 presence: true
   validates :info,                 presence: true
   validates :category_id,          presence: true
