5bca84e940bda5d0aef4152578422a72315b52fc
okayu0220 <y.okajima.0220@gmail.com>
2023-08-01 18:05:11 +0900

    active_hashデータとのアソシエーションとバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,22 @@
 class Item < ApplicationRecord
 
-  belogs_to :user
+  belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :fee_status
+  belongs_to :prefecture
+  belongs_to :delivery_schedule
 
   validates :image,                presence: true
   validates :name,                 presence: true
   validates :info,                 presence: true
-  validates :category_id,          presence: true
-  validates :item_status_id,       presence: true
-  validates :fee_status_id,        presence: true
-  validates :prefecture_id,        presence: true
-  validates :delivery_schedule_id, presence: true
+  validates :category_id,          presence: true, numericality: {other_than: 0, message: "can't be blank"}
+  validates :item_status_id,       presence: true, numericality: {other_than: 0, message: "can't be blank"}
+  validates :fee_status_id,        presence: true, numericality: {other_than: 0, message: "can't be blank"}
+  validates :prefecture_id,        presence: true, numericality: {other_than: 0, message: "can't be blank"}
+  validates :delivery_schedule_id, presence: true, numericality: {other_than: 0, message: "can't be blank"}
   validates :price,                presence: true
 end
