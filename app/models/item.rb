f9924bdb7a4ce14948e0550059b6b4cdbccb5698
okayu0220 <y.okajima.0220@gmail.com>
2023-08-02 14:34:39 +0900

    販売価格のバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,5 +18,5 @@ class Item < ApplicationRecord
   validates :fee_status_id,        presence: true, numericality: {other_than: 0, message: "can't be blank"}
   validates :prefecture_id,        presence: true, numericality: {other_than: 0, message: "can't be blank"}
   validates :delivery_schedule_id, presence: true, numericality: {other_than: 0, message: "can't be blank"}
-  validates :price,                presence: true
+  validates :price,                presence: true, format: {with: /\A[\d]{3,7}\z/}, numericality: {in: 300..9999999 }  # 半角数字かつ300~9,999,999のみ保存可能
 end
