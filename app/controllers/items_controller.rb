0ca9e55274abfa7f3cd82acdbc21f36197810dec
okayu0220 <y.okajima.0220@gmail.com>
2023-08-04 11:19:39 +0900

    編集機能のためのルーティングとアクションを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,22 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    if user_signed_in? && current_user.id != @item.user.id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def item_params
@@ -43,4 +59,5 @@ class ItemsController < ApplicationController
       redirect_to new_user_session_path
     end
   end
+
 end
