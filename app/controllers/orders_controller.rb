4f9de31a5a58cfffd4b083fe885822780699828b
okayu0220 <y.okajima.0220@gmail.com>
2023-08-05 18:23:00 +0900

    ログイン状況による購入画面へのアクセス制限設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,27 @@
 class OrdersController < ApplicationController
+  before_action :redirect_to_sign_in
+
   def index
+    @item = Item.find(params[:item_id])
+    @order = Order.new
+    user_check
   end
   
   def create
   end
+
+  private
+
+  def redirect_to_sign_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+  def user_check
+    if user_signed_in? && current_user.id == @item.user.id
+      redirect_to root_path
+    end
+  end
+
 end
