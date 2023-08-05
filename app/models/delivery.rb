d1f5ab4491beb2cc0d85d193ceabf84defae3b42
okayu0220 <y.okajima.0220@gmail.com>
2023-08-05 17:56:28 +0900

    モデル間のアソシエーションとバリデーションを設定

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,2 +1,14 @@
 class Delivery < ApplicationRecord
+  belongs_to :order
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
+  with_options presence: true do
+    validates :post_code,       format: { with: /\A[\d]{3}-[\d]{4}\z/ }
+    validates :prefecture_id,   numericality: { other_than: 0, message: "can't be blank" }
+    validates :municipalities
+    validates :house_number
+    validates :phone_number,    format: { with: /\A[\d]{10,11}\z/ }
+  end
 end
